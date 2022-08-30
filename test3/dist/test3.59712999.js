68fba8594ccf8377f090c80ababce4dba67f0059
Mikalai Vegera <wilshere10000@gmail.com>
2022-08-30 19:27:32 +0300

    сhange settings package json

--- test3/dist/test3.59712999.js
+++ test3/dist/test3.59712999.js
@@ -533,6 +533,6 @@ function hmrAcceptRun(bundle, id) {
 
 },{}],"4M6V8":[function(require,module,exports) {
 
-},{}]},["lX4BT","4M6V8"], "4M6V8", "parcelRequire2c60")
+},{}]},["lX4BT","4M6V8"], "4M6V8", "parcelRequire2c0c")
 
-//# sourceMappingURL=activebox.59712999.js.map
+//# sourceMappingURL=test3.59712999.js.map
