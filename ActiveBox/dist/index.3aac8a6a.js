788bbda88be58184425b900293ad34d80c019d4a
Mikolka <wilshere10000@gmail.com>
2022-09-12 13:05:03 +0300

    build activebox

---
+++ ActiveBox/dist/index.3aac8a6a.js
@@ -0,0 +1,2 @@
+function scrollFunction(){document.body.scrollTop>525||document.documentElement.scrollTop>525?document.getElementById("header").style.backgroundColor="#353C65":document.getElementById("header").style.backgroundColor="transparent"}window.onscroll=function(){scrollFunction()};
+//# sourceMappingURL=index.3aac8a6a.js.map
